693be33c1615c492d63282f5085f7e4877f0e805
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2017-02-18 16:52:00 -0200

    End of Class

--- src/br/com/caelum/financas/service/Agendador.java
+++ src/br/com/caelum/financas/service/Agendador.java
@@ -1,9 +1,22 @@
 package br.com.caelum.financas.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.ejb.ScheduleExpression;
+import javax.ejb.Stateless;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
+
+@Stateless
 public class Agendador {
 
 	private static int totalCriado;
-
+	@Resource
+	private TimerService timerService;
+	
 	public void executa() {
 		System.out.printf("%d instancias criadas %n", totalCriado);
 
@@ -14,5 +27,42 @@ public class Agendador {
 		} catch (InterruptedException e) {
 		}
 	}
-
+	
+	@PostConstruct
+	public void postConstruct()
+	{
+			System.out.println("Criando Agendador");
+			totalCriado++;
+	}
+	
+	@PreDestroy
+	public void preDestroy()
+	{
+		System.out.println("Destruindo agendador");
+	}
+	
+	
+	public void agenda(String expressaoMinutos, String expressaoSegundos)
+	{
+		ScheduleExpression expression = new ScheduleExpression();
+		expression.hour("*");
+		expression.minute(expressaoMinutos);
+		expression.second(expressaoSegundos);
+		
+		TimerConfig config = new TimerConfig();
+		config.setInfo(expression.toString());
+		config.setPersistent(false);
+		
+		this.timerService.createCalendarTimer(expression, config);
+		
+		System.out.println("Agendamento : "+expression);
+		
+		
+	}
+	
+	@Timeout
+	public void verificacaoPeriodoSeNaNovasContas(Timer timer)
+	{
+		System.out.println(timer.getInfo());
+	}
 }
